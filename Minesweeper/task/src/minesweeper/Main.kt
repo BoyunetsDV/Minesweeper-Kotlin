ed2eed701e533830dd6edfe7a48421a109bbb672
Boiunets Denys <boyunets.dv@gmail.com>
2020-11-03 18:27:54 +0200

    minesweeper complete and all stages are complete

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -7,6 +7,7 @@ class Minesweeper {
     var mineField: Array<Array<String>> = emptyArray()
     var visibleMineField: Array<Array<String>> = emptyArray()
     var isGameEnded: Boolean = false
+    var isGameOver: Boolean = false
     var minesCount = 0
 
     companion object {
@@ -24,6 +25,7 @@ class Minesweeper {
             makeTurn()
             checkIfGameIsEnded()
         }
+        displayVisibleMineField()
         displayResult()
     }
 
@@ -43,16 +45,19 @@ class Minesweeper {
     }
 
     private fun fillWithMines() {
+        val fields = mutableListOf<Pair<Int, Int>>()
+        for (i in 0 until HEIGHT) {
+            for (j in 0 until WIDTH) {
+                fields.add(Pair(i, j))
+            }
+        }
         for (k in 0 until minesCount) {
-            do {
-                var isMinePlaced = false
-                val x = Random.nextInt(0, WIDTH)
-                val y = Random.nextInt(0, HEIGHT)
-                if (mineField[y][x] == ".") {
-                    mineField[y][x] = "X"
-                    isMinePlaced = true
-                }
-            } while (!isMinePlaced)
+            if (fields.isEmpty()) {
+                break
+            }
+            val minePoint = fields.random()
+            fields.remove(minePoint)
+            mineField[minePoint.first][minePoint.second] = "X"
         }
     }
 
@@ -60,7 +65,6 @@ class Minesweeper {
         for (i in mineField.indices) {
             for (j in mineField[i].indices) {
                 if (mineField[i][j] == "X") {
-                    visibleMineField[i][j] = "."
                     continue
                 }
                 var nearMinesCount = 0
@@ -74,12 +78,10 @@ class Minesweeper {
                 if (i + 1 < HEIGHT && j - 1 >= 0 && mineField[i + 1][j - 1] == "X") nearMinesCount++
                 if (i + 1 < HEIGHT && mineField[i + 1][j] == "X") nearMinesCount++
                 if (i + 1 < HEIGHT && j + 1 < WIDTH && mineField[i + 1][j + 1] == "X") nearMinesCount++
-                if (nearMinesCount > 0) {
-                    mineField[i][j] = nearMinesCount.toString()
-                    visibleMineField[i][j] = nearMinesCount.toString()
+                mineField[i][j] = if (nearMinesCount > 0) {
+                    nearMinesCount.toString()
                 } else {
-                    mineField[i][j] = "."
-                    visibleMineField[i][j] = "."
+                    "."
                 }
             }
         }
@@ -87,35 +89,74 @@ class Minesweeper {
 
     private fun makeTurn() {
         while (true) {
-            print("Set/delete mines marks (x and y coordinates): ")
-            val y = scanner.nextInt() - 1
+            print("Set/unset mines marks or claim a cell as free: ")
             val x = scanner.nextInt() - 1
-            if (visibleMineField[x][y] != "." && visibleMineField[x][y] != "*") {
-                println("There is a number here!")
-            } else if (visibleMineField[x][y] == ".") {
-                visibleMineField[x][y] = "*"
-                break
-            } else {
-                visibleMineField[x][y] = "."
-                break
+            val y = scanner.nextInt() - 1
+            val turnType = scanner.next()
+
+            when {
+                mineField[y][x] == "X" && turnType == "free" -> addAllMinesToDisplay()
+                visibleMineField[y][x][0].isDigit() && turnType == "free" -> {
+                    println("There is a number here!")
+                    continue
+                }
+                mineField[y][x][0].isDigit() && turnType == "free" -> visibleMineField[y][x] = mineField[y][x]
+                visibleMineField[y][x] == "/" && turnType == "free" -> {
+                    println("There is a / here!")
+                    continue
+                }
+                visibleMineField[y][x] == "." && turnType == "free" -> makeCellsVisible(y, x)
+                visibleMineField[y][x] == "*" && turnType == "mine" -> visibleMineField[y][x] = "."
+                visibleMineField[y][x] == "." && turnType == "mine" -> visibleMineField[y][x] = "*"
             }
+            break
         }
     }
 
-    private fun checkIfGameIsEnded() {
-        var marksCount = 0
-        var minesMarked = 0
-        for (i in visibleMineField.indices) {
-            for (j in visibleMineField[i].indices) {
-                if (visibleMineField[i][j] == "*" && mineField[i][j] == "X") {
-                    minesMarked++
-                    marksCount++
-                } else if (visibleMineField[i][j] == "*") {
-                    marksCount++
+    private fun addAllMinesToDisplay() {
+        for (i in mineField.indices) {
+            for (j in mineField[i].indices) {
+                if (mineField[i][j] == "X") {
+                    visibleMineField[i][j] = "X"
                 }
             }
         }
-        isGameEnded = marksCount == minesMarked && marksCount == minesCount
+        isGameOver = true
+    }
+
+    private fun makeCellsVisible(x: Int, y: Int) {
+        if (x < 0 || y < 0 || x >= WIDTH || y >= HEIGHT || visibleMineField[x][y] !in ".*") {
+            return
+        }
+        if (mineField[x][y][0].isDigit()) {
+            visibleMineField[x][y] = mineField[x][y]
+            return
+        }
+        visibleMineField[x][y] = "/"
+        makeCellsVisible(x - 1, y - 1)
+        makeCellsVisible(x - 1, y)
+        makeCellsVisible(x - 1, y + 1)
+        makeCellsVisible(x, y - 1)
+        makeCellsVisible(x, y + 1)
+        makeCellsVisible(x + 1, y - 1)
+        makeCellsVisible(x + 1, y)
+        makeCellsVisible(x + 1, y + 1)
+    }
+
+    private fun checkIfGameIsEnded() {
+        if (isGameOver) {
+            isGameEnded = true
+            return
+        }
+        var unOpened = 0
+        for (i in visibleMineField.indices) {
+            for (j in visibleMineField[i].indices) {
+                if (visibleMineField[i][j] == "*" || visibleMineField[i][j] == ".") {
+                    unOpened++
+                }
+            }
+        }
+        isGameEnded = unOpened == minesCount
     }
 
     private fun displayVisibleMineField() {
@@ -127,7 +168,9 @@ class Minesweeper {
     }
 
     private fun displayResult() {
-        if (isGameEnded) {
+        if (isGameOver) {
+            println("You stepped on a mine and failed!")
+        } else {
             println("Congratulations! You found all the mines!")
         }
     }
